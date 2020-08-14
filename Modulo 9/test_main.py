99081bf66a4ff612e9719ae70d71a65393a56845
RogerioLS <rogerio_288@hotmail.com>
2020-08-14 14:54:09 -0300

    excluir modulo 9

--- Modulo 9/test_main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def test_0():
-    answer = pd.read_csv("answer.csv")
-    assert answer.shape == (4570, 2) 
-    assert set(["NU_INSCRICAO", "IN_TREINEIRO"]) == set(answer.columns)
